58be3febd015fd2b7178de9c46474a53481915d3
rajeev ranjan <rajeev@infrrd.ai>
2018-08-21 14:41:40 +0530

    option menu added

--- app/src/main/java/infrrd/ai/nevernote/MainActivity.kt
+++ app/src/main/java/infrrd/ai/nevernote/MainActivity.kt
@@ -2,11 +2,35 @@ package infrrd.ai.nevernote
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater:MenuInflater = getMenuInflater()
+        menuInflater.inflate(R.menu.options_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item:MenuItem):Boolean{
+        return when(item.itemId){
+            R.id.sort -> {
+                Toast.makeText(this, "sort button is clicked", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.settings -> {
+                Toast.makeText(this, "settings button is clicked", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
