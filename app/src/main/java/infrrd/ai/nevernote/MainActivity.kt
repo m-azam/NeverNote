8f47b8f69e071d8e5417648c98900b6400c44116
Mohammed Azam <mazam1488@gmail.com>
2018-08-21 11:49:41 +0530

    first commit

---
+++ app/src/main/java/infrrd/ai/nevernote/MainActivity.kt
@@ -0,0 +1,12 @@
+package infrrd.ai.nevernote
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
