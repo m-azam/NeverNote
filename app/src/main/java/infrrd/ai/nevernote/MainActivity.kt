72f975c0df1f77826f44e7cf9f67ee7257963f1c
NikhilL96 <nikhil96.mail@gmail.com>
2018-08-22 15:29:41 +0530

    this commit will add navigation bar options

--- app/src/main/java/infrrd/ai/nevernote/MainActivity.kt
+++ app/src/main/java/infrrd/ai/nevernote/MainActivity.kt
@@ -1,9 +1,11 @@
 package infrrd.ai.nevernote
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBar
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuInflater
@@ -11,7 +13,7 @@ import android.view.MenuItem
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener  {
 
     private lateinit var drawerLayout: DrawerLayout
 
@@ -20,11 +22,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         drawerLayout = drawer_layout
+
         val actionbar: ActionBar? = supportActionBar
         actionbar?.apply {
             setDisplayHomeAsUpEnabled(true)
             setHomeAsUpIndicator(R.drawable.ic_menu_black)
         }
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -33,15 +43,24 @@ class MainActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.sort -> {
                 Toast.makeText(this, "sort button is clicked", Toast.LENGTH_SHORT).show()
-                return true
+                true
             }
             R.id.settings -> {
                 Toast.makeText(this, "settings button is clicked", Toast.LENGTH_SHORT).show()
-                return true
+                true
             }
             android.R.id.home -> {
                 drawerLayout.openDrawer(GravityCompat.START)
@@ -49,5 +68,24 @@ class MainActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.trash -> {
+
+            }
+            R.id.completed -> {
+
+            }
+            R.id.all_notes -> {
+
+            }
+
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
