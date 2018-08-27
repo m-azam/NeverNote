b6822b898cd4b54b9e253e679c6076345b40e56b
Mohammed Azam <mazam1488@gmail.com>
2018-08-27 12:53:30 +0530

    Added BaseActivity

---
+++ app/src/main/java/infrrd/ai/nevernote/BaseActivity.kt
@@ -0,0 +1,76 @@
+package infrrd.ai.nevernote
+
+import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.base_activity.*
+
+abstract class BaseActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.base_activity)
+        layoutInflater.inflate(getContentView(), layout_container, true)
+        setSupportActionBar(toolbar)
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+        nav_view.setNavigationItemSelectedListener(this)
+
+    }
+
+    abstract fun getContentView(): Int
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater: MenuInflater = getMenuInflater()
+        menuInflater.inflate(R.menu.options_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.sort -> {
+                Toast.makeText(this, "sort button is clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.settings -> {
+                Toast.makeText(this, "settings button is clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.trash -> {
+                Toast.makeText(this, "trash button clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.completed -> {
+                Toast.makeText(this, "completed button clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.all_notes -> {
+                Toast.makeText(this, "all_notes button clicked", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
--- app/src/main/java/infrrd/ai/nevernote/MainActivity.kt
+++ app/src/main/java/infrrd/ai/nevernote/MainActivity.kt
@@ -10,70 +10,17 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener  {
+class MainActivity : BaseActivity()  {
+    override fun getContentView(): Int {
+        return R.layout.activity_main
+    }
 
     private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-        drawerLayout = drawer_layout
-
-        val toggle = ActionBarDrawerToggle(
-                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        nav_view.setNavigationItemSelectedListener(this)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val menuInflater: MenuInflater = getMenuInflater()
-        menuInflater.inflate(R.menu.options_menu, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onBackPressed() {
-        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-            drawer_layout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
     }
 
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.sort -> {
-                Toast.makeText(this, "sort button is clicked", Toast.LENGTH_SHORT).show()
-                true
-            }
-            R.id.settings -> {
-                Toast.makeText(this, "settings button is clicked", Toast.LENGTH_SHORT).show()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.trash -> {
-                Toast.makeText(this, "trash button clicked", Toast.LENGTH_SHORT).show()
-            }
-            R.id.completed -> {
-                Toast.makeText(this, "completed button clicked", Toast.LENGTH_SHORT).show()
-            }
-            R.id.all_notes -> {
-                Toast.makeText(this, "all_notes button clicked", Toast.LENGTH_SHORT).show()
-            }
-
-        }
-
-        drawer_layout.closeDrawer(GravityCompat.START)
-        return true
-    }
 }
