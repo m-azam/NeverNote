237f81cf0141e7afa2805e13d32b6952c5f4d20d
rajeev ranjan <rajeev@infrrd.ai>
2018-08-28 16:22:58 +0530

    validation added in signup screen added

--- app/src/main/java/infrrd/ai/nevernote/SignupActivity.kt
+++ app/src/main/java/infrrd/ai/nevernote/SignupActivity.kt
@@ -2,7 +2,12 @@ package infrrd.ai.nevernote
 
 import android.content.Intent
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_signup.signup_email_id
 import kotlinx.android.synthetic.main.activity_signup.signup_button
+import kotlinx.android.synthetic.main.activity_signup.signup_password
+import kotlinx.android.synthetic.main.activity_signup.signup_confirm_password
+import java.util.regex.Pattern
 
 class SignupActivity : BaseActivity() {
 
@@ -10,8 +15,12 @@ class SignupActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
         signup_button.setOnClickListener {
-//            Write API for signup here
-            goToLoginScreen()
+            var emailValidatorFlag = confirmPassword()
+            var passwordValidatorFlag = validateEmail()
+            if(emailValidatorFlag && passwordValidatorFlag){
+//                API for signup here
+                goToLoginScreen()
+            }
         }
     }
 
@@ -21,5 +30,30 @@ class SignupActivity : BaseActivity() {
 
     fun goToLoginScreen(){
         finish()
+    }
+
+    fun confirmPassword(): Boolean {
+        var returnValue = true;
+        if(signup_password.text.isNullOrEmpty()) {
+            signup_password.error = "Field can't be left empty"
+            returnValue = false
+        }
+        if(signup_confirm_password.text.isNullOrEmpty()) {
+            signup_confirm_password.error = "Field can be left empty"
+            returnValue = false
+        }
+        if(returnValue) return signup_password.text == signup_confirm_password.text else return false   
+    }
+
+    fun validateEmail(): Boolean {
+        if (signup_email_id.text.isNullOrEmpty()) {
+            signup_email_id.error = "Field can't be left empty"
+            return false
+        }
+        else if (!Pattern.matches(".+@[a-zA-Z]+\\.com", signup_email_id.text)) {
+            signup_email_id.error = "Not a valid email"
+            return false
+        }
+        return true
     }
 }
